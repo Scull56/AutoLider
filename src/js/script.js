187dd3ce1532e9a21f9de9dcd5ff3aff0f878450
[Daniil_Inozemcev] <daniilscull56@yandex.ru>
2021-03-16 22:12:26 +0300

    best modify

--- src/js/script.js
+++ src/js/script.js
@@ -21,55 +21,63 @@ document.addEventListener('DOMContentLoaded', () => {
    });
 
    // Slider
-   const slider = document.querySelector('.slider'),
-      sliderTrack = slider.querySelector('.slider__track'),
-      sliderItems = slider.querySelectorAll('.slider__item'),
-      sliderNext = slider.querySelector('.slider__arrow_next'),
-      sliderPrev = slider.querySelector('.slider__arrow_prev'),
-      sliderCounter = slider.querySelector('.slider__counter');
+   function initSlider(sliderClass) {
+      const slider = document.querySelector(`${sliderClass}`),
+         sliderTrack = slider.querySelector('.slider__track'),
+         sliderItems = slider.querySelectorAll('.slider__item'),
+         sliderNext = slider.querySelector('.slider__arrow_next'),
+         sliderPrev = slider.querySelector('.slider__arrow_prev'),
+         sliderCounter = slider.querySelector('.slider__counter');
 
-   // Счётчик текущего слайда
-   let slideCurrent = 1;
+      // Счётчик текущего слайда
+      let slideCurrent = 1;
 
-   // Ширина дорожки со слайдами
-   sliderTrack.style.width = `${(100 * sliderItems.length)}%`;
+      // Ширина дорожки со слайдами
+      sliderTrack.style.width = `${(100 * sliderItems.length)}%`;
 
-   // Ширина слайда в дорожке
-   const slideWidth = 100 / sliderItems.length;
-   sliderItems.forEach(item => {
-      item.style.width = `${slideWidth}%`;
-   });
+      // Ширина слайда в дорожке
+      const slideWidth = 100 / sliderItems.length;
+      sliderItems.forEach(item => {
+         item.style.width = `${slideWidth}%`;
+      });
 
-   // Смещение слайда
-   sliderTrack.style.transform = `translateX(-${(slideCurrent - 1) * slideWidth}%)`;
+      // Смещение слайда
+      sliderTrack.style.transform = `translateX(-${(slideCurrent - 1) * slideWidth}%)`;
 
-   // Инициализация количества слайдов в слайдере в счётчике
-   sliderCounter.textContent = `${slideCurrent}/${sliderItems.length}`;
-
-   function changeSlide(dir) {
-      if (dir == 'r') {
-         slideCurrent += 1;
-      } else if (dir == 'l') {
-         slideCurrent -= 1;
-      }
-
-      if (slideCurrent === 0) {
-         slideCurrent = sliderItems.length;
-      } else if (slideCurrent === sliderItems.length + 1) {
-         slideCurrent = 1;
-      }
-
-      const offset = (slideCurrent - 1) * slideWidth;
-      sliderTrack.style.transform = `translateX(-${offset}%)`;
-
+      // Инициализация количества слайдов в слайдере в счётчике
       sliderCounter.textContent = `${slideCurrent}/${sliderItems.length}`;
+
+      function changeSlide(dir) {
+         if (dir == 'r') {
+            slideCurrent += 1;
+         } else if (dir == 'l') {
+            slideCurrent -= 1;
+         }
+
+         if (slideCurrent === 0) {
+            slideCurrent = sliderItems.length;
+         } else if (slideCurrent === sliderItems.length + 1) {
+            slideCurrent = 1;
+         }
+
+         const offset = (slideCurrent - 1) * slideWidth;
+         sliderTrack.style.transform = `translateX(-${offset}%)`;
+
+         sliderCounter.textContent = `${slideCurrent}/${sliderItems.length}`;
+      }
+
+      sliderNext.addEventListener('click', () => {
+         changeSlide('r');
+      });
+
+      sliderPrev.addEventListener('click', () => {
+         changeSlide('l');
+      });
    }
 
-   sliderNext.addEventListener('click', () => {
-      changeSlide('r');
-   });
+   // Mainscreen Slider
+   initSlider('.mainscreen__slider');
 
-   sliderPrev.addEventListener('click', () => {
-      changeSlide('l');
-   });
+   // Best Slider
+   initSlider('.best__slider');
 });

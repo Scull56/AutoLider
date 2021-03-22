7fcc38c2568acc65a9626c255b5be7d6bf714f1a
[Daniil_Inozemcev] <daniilscull56@yandex.ru>
2021-03-22 14:47:45 +0300

    best modify about mofify

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,11 @@ const gulp = require('gulp'),
    htmlmin = require('gulp-htmlmin');
 
 gulp.task('server', function () {
-
    browserSync({
       server: {
          baseDir: "src"
       }
    });
-
    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
--- src/js/script.js
+++ src/js/script.js
@@ -75,9 +75,22 @@ document.addEventListener('DOMContentLoaded', () => {
       });
    }
 
+   // IBG 
+   function ibg() {
+      let ibg = document.querySelectorAll(".ibg");
+      for (var i = 0; i < ibg.length; i++) {
+         if (ibg[i].querySelector('img')) {
+            ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+         }
+      }
+   }
+
    // Mainscreen Slider
    initSlider('.mainscreen__slider');
 
    // Best Slider
    initSlider('.best__slider');
+
+   // IBG
+   ibg();
 });
